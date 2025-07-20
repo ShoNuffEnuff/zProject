a3e62541f61eea3031d08d4390fceccdb84022a5
ShoNuffEnuff <108638570+ShoNuffEnuff@users.noreply.github.com>
2025-07-20 18:47:25 +0930

    Fleeb

--- zProject.py
+++ zProject.py
@@ -97,7 +97,7 @@ def logout():
     session.clear()
     return jsonify({"message": "Logged out successfully."})
 
-if __name__ == "__main__":
-    if __name__ == "__main__":
-        app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+# if __name__ == "__main__":
+#     if __name__ == "__main__":
+#         app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
 
