e0383265b6a63bd5c51ded6bea421e149a9a2648
ShoNuffEnuff <108638570+ShoNuffEnuff@users.noreply.github.com>
2025-07-20 16:48:09 +0930

    Render/Githubpages

--- zProject.py
+++ zProject.py
@@ -98,4 +98,6 @@ def logout():
     return jsonify({"message": "Logged out successfully."})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    if __name__ == "__main__":
+        app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
